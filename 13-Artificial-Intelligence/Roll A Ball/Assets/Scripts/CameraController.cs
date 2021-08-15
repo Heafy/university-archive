3702842094f22a825833966dda2a33752d83ec2d
Jorge Martinez <jmtz.2878@gmail.com>
2021-08-15 16:53:10 -0500

    Missing practice

---
+++ 13-Artificial-Intelligence/Roll A Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Player del juego
+	public GameObject player;
+
+	// Distancia entre el player y la camara
+	private Vector3 offset; 
+
+	// Use this for initialization
+	void Start () {
+		// Coloca la camara en posicion
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		// Actualiza la posicion de la camara
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ 13-Artificial-Intelligence/Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	// La velocidad a la que se mueve Player
+	public float speed;
+	// Text para mostrar los cubos recogidos
+	public Text countText;
+	// Text para mostrar cuando el jugador gana
+	public Text winText;
+	// Rigidbody para Player
+	private Rigidbody rb;
+	// Int para contar los cubos recogidos
+	private int count;
+	
+	// Inicializa los componentes
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText();
+		winText.text = "";
+	}
+
+	// Le da el movimiento correcto a Player
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement*speed);
+	}
+
+	// Cuando detecta una colision con un objeto Pickup aumenta la cuenta
+	// y desaparece el objeto de la escena
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			setCountText();
+		}
+	}
+
+	// Mantiene la informacion actualizada de los cubos y muestra el 
+	// mensaje ganador
+	void setCountText(){
+		countText.text = "Cubos recogidos: " + count.ToString();
+		if(count >= 12){
+			winText.text = "¡Ganaste!";
+		} 
+	}
+}
